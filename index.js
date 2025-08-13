ad9df70c6b73f21ca8b454a91aa20667a7d96c0f
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-13 11:17:36 +0530

    add

--- index.js
+++ index.js
@@ -16,27 +16,27 @@ function getfeedHtml(){
     for(let tweet of tweetsData){
      feedHtml +=`<div class="tweet">
     <div class="tweet-inner">
-        <img src="URL OF PROFILE PIC" class="profile-pic">
+        <img src="${tweet.profilePic}" class="profile-pic">
         <div>
-            <p class="handle">TWEET HANDLE</p>
-            <p class="tweet-text">TWEET TEXT</p>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-                    NUMBER OF REPLIES
+                    ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                    NUMBER OF LIKES
+                    ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    NUMBER OF RETWEETS
+                    ${tweet.retweets}
                 </span>
             </div>   
         </div>            
     </div>
 </div>`   
 }
-feedHtml.innerHtml=feedHtml
+feed.innerHTML=feedHtml
 }
 
-
+getfeedHtml()
 
