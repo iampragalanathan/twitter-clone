4d44d89e0d95306b96cfe6e3a408a08d4f781386
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-13 10:47:51 +0530

    add

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import { tweetsData } from "./data.js";
 console.log(tweetsData)
+const tweetInput=document.getElementById("tweet-input")
+const twitButton=document.getElementById("tweet-btn")
+
+twitButton.addEventListener("click",function(){
+     
+    console.log(tweetInput.value)
+    tweetInput.value=""
+})
